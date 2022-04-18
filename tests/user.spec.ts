05a484e2a61aa21a09142adb8101fde35f17baea
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-04-18 19:22:27 +0100

    test: first user tests :test_tube:

---
+++ tests/user.spec.ts
@@ -0,0 +1,32 @@
+import 'mocha';
+import {expect} from 'chai';
+import {Note} from '../src/note';
+import {User} from '../src/user';
+
+const blueNote: Note = new Note('Blue Note', 'This is a blue note', 'Blue');
+const redNote: Note = new Note('Red Note', 'This is a red note', 'Red');
+const greenNote: Note = new Note('Green Note', 'This is a green note', 'Green');
+const firstUser: User = new User('user', [blueNote, redNote]);
+
+describe('User Class tests', () => {
+  describe('User Class getters', () => {
+    it('username getter', () => {
+      expect(firstUser.name).to.be.eql('user');
+    });
+    it('Notes getter', () => {
+      expect(firstUser.notes).to.be.eql([blueNote, redNote]);
+    });
+  });
+  describe('User Class setters', () => {
+    it('title setter', () => {
+      firstUser.name = 'firstUser';
+      expect(firstUser.title).to.be.eql('firstUser');
+    });
+    it('Notes setter', () => {
+      firstUser.notes = [greenNote, blueNote];
+      expect(firstUser.notes).to.be.eql([greenNote, blueNote]);
+    });
+  });
+  describe('User Class methods', () => {
+  });
+});
