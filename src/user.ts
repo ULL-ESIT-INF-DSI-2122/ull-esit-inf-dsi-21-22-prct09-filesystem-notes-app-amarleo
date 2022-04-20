b698b9333785e3ca86ad692456b76c9a3f573f6e
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-04-20 13:15:27 +0100

    feat: add note to user method update :gear:

--- src/user.ts
+++ src/user.ts
@@ -7,10 +7,27 @@ export class User {
 
   constructor(
       name: string,
-      notes: Note[],
+      notes: Note[] = [],
   ) {
     this._name = name;
     this._notes = notes;
+    notes.forEach((element: Note) => {
+      const databasePath: string = './database/' + this._name;
+      const jsonPath: string = databasePath + '/' + element.title + '.json';
+      if (!fs.existsSync('./database')) {
+        fs.mkdirSync('./database');
+        console.log('\nSe ha creado el directorio database');
+      }
+      if (!fs.existsSync(databasePath)) {
+        fs.mkdirSync(databasePath);
+        console.log(`\nSe ha creado el directorio al usuario ${this._name}`);
+      }
+      if (!fs.existsSync(jsonPath)) {
+        fs.openSync(jsonPath, 'w');
+        console.log(`\nSe ha añadido al usuario` +
+          ` ${this._name} la nota ${element.title}`);
+      }
+    });
   }
 
   get name(): string {
@@ -31,11 +48,22 @@ export class User {
 
   addNote(note: Note) {
     const databasePath: string = './database/' + this._name;
-    const jsonPath: string = databasePath + '/' + note.title; + '.json';
-
+    const jsonPath: string = databasePath + '/' + note.title + '.json';
+    if (!fs.existsSync('./database')) {
+      fs.mkdirSync('./database');
+      console.log('\nSe ha creado el directorio database');
+    }
     if (!fs.existsSync(databasePath)) {
-      console.log();
       fs.mkdirSync(databasePath);
+      console.log(`\nSe ha creado el directorio al usuario ${this._name}`);
+    }
+    if (fs.existsSync(jsonPath)) {
+      console.log(`\nError: La nota ${note.title} ya existe` +
+        ` para el usuario ${this._name}`);
+    } else {
+      fs.openSync(jsonPath, 'w');
+      console.log(`\nSe ha añadido al usuario` +
+        ` ${this._name} la nota ${note.title}`);
     }
   }
 }
@@ -43,5 +71,5 @@ export class User {
 const firstNote: Note = new Note('Blue Note', 'This is a blue note', 'Blue');
 const redNote: Note = new Note('Red Note', 'This is a red note', 'Red');
 const user: User = new User('Ale', [firstNote]);
-
+const fifa: User = new User('Fifa', [redNote]);
 user.addNote(redNote);
