a81dc73dd08699b20597e7045ff643b50f9d2424
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-04-24 17:41:37 +0100

    gear: merge from user + index file created :gear:

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import {Note} from './note';
+import {User} from './user';
+import * as yargs from 'yargs';
+
+function run() {
+  yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+      title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string',
+      },
+    },
+    handler(argv) {
+      if (typeof argv.title === 'string') {
+        // Required logic to add a new note
+      }
+    },
+  });
+}
